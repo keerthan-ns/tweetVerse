1b48b4344ac0ee5f7807b442dac9c8955a739135
Keerthan N S <keerthanns091@gmail.com>
2023-03-01 22:22:31 -0800

    [update] migrated to VM

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -1,19 +1,3 @@
-// class AppwriteConstants {
-//   static const String databaseId = '63faa62859f7c938adc2';
-//   static const String projectID = '63faa404dd2a3aba1606';
-//   // static const String endPoint = 'http://localhost:80/v1';
-//   static const String endPoint = 'http://192.168.148.42:80/v1';
-
-//   static const String usersCollection = '63fbb83eefb822e1d403';
-//   static const String tweetsCollection = '63fc3365de1fd1737bfb';
-//   static const String notificationsCollection = '63fea8d16de34c042fa7';
-
-//   static const String imagesBucket = '63fc3ef1a64a7e498085';
-
-//   static String imageUrl(String imageId) =>
-//       '$endPoint/storage/buckets/$imagesBucket/files/$imageId/view?project=$projectID&mode=admin';
-// }
-
 class AppwriteConstants {
   static const String databaseId = '63ff43b37f21fc071917';
   static const String projectID = '63ff423f8bd01a2858f2';
